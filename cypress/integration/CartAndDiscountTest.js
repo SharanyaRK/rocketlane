4669c1fff7864ac6c1de3a2ad6f97cdabfc1603d
SharanyaRK <sharanyaece75@gmail.com>
2022-01-10 14:09:39 +0530

    added utils and custom commands

--- cypress/integration/CartAndDiscountTest.js
+++ cypress/integration/CartAndDiscountTest.js
@@ -1,68 +1,60 @@
+import * as util from '../support/utils.js';
+
+
 describe('Test case to check add to cart and discount', () => {
-  it('Add to cart test', () => {
-    // load base url
+  beforeEach(function () {
     cy.visit('/')
-    // goto chairs page and assert url
-    cy.get('a[href*="/department/chairs"]').first().click()
-    cy.url().should('include', '/department/chairs')
-    // validation for out of stock chairs 
-    cy.xpath("//div[@class='status status--red']/..").should(($outOfStock) => {
-      $outOfStock.get(1).click()
-    })
-    cy.xpath("//*[@class='modal-sold-out modal-sold-out--is-open']//*[.='Continue shopping']")
-    .should('have.text','Continue shopping').click()
-    // checking for the first chair in stock and adding to cart
-    cy.xpath("//div[@class='product-card-container']//a[contains(@href,'product/')]//div//div").first().should('have.text','In stock').click()
-    cy.url().should('include', '/chairs/product/')
-    cy.xpath("//*[@class='purchase-button']").click()
-    // assert cart has 1 product
-    cy.xpath("//a[contains(@href,'cart')]//div[@class='menu-item-large']").should('have.text', 'Cart (1)')
-
-    // goto sofa section
-    cy.get('a[href*="/department/sofas"]').first().click()
-    cy.url().should('include', '/department/sofas')
-    // select a sofa to add to cart
-    const sofaName = "Black Velvet Sectional"
-    cy.xpath("//div[@class='description']//div[contains(.,\""+sofaName+"\")]/../..").click()
-    cy.url().should('include', '/sofas/product/')
-    cy.xpath("//*[@class='purchase-button']").click()
-    // assert cart has 2 product
-    cy.xpath("//a[contains(@href,'cart')]//div[@class='menu-item-large']").should('have.text', 'Cart (2)').click()
-
-    //verifying items in cart
-    cy.xpath("//*[.='Items']/../..//div//div[@class='product']//div[@class='product-description']//div[contains(.,'Chair')]").should('exist');
-    cy.xpath("//*[.='Items']/../..//div//div[@class='product']//div[@class='product-description']//div[contains(.,\""+sofaName+"\")]").should('exist');
-
-
-    // assertions for chair price and sofa price
-    cy.get('.product-price').should(($price) => {
-      let chairPrice = $price.get(0).innerText;
-      let sofaPrice = $price.get(1).innerText;
-      expect(chairPrice).to.eq("$250.00")
-      expect(sofaPrice).to.eq("$1450.00")
-
-    })
-    // assertion for order value 
-    cy.xpath("//*[@class='summary']//div[@class='lines']//div[.='Order value']/../div").should(($orderValue) => {
-      let totalPrice = $orderValue.get(1).innerText;
-      expect(totalPrice).to.eq("$1700.00")
-    })
-    // assertion for total price
-    cy.xpath("//div[@class='line line-total']//div[.='Total']/../div").should(($total) => {
-      let totalPrice = $total.get(1).innerText;
-      expect(totalPrice).to.eq("1927.00")
+    cy.fixture('inputdata').then((input) => {
+      this.input = input
     })
   })
 
- it('Discount coupon test', () => {
-     //visit base url and goto cart section
-     cy.visit('/')
-     cy.get('.cart').click()
-     // Adding random discount code
-     cy.get('.discount').type("asdad")
-     cy.xpath("//div[@class='discount']//div[contains(.,'Apply')]").click()
-     // assert error text
-     cy.get('.discount-toast').should('have.text', 'Coupon is invalid')
+  it('Add to cart test', function () {
+
+    // goto chairs page and assert url
+    cy.get('a[href*="/department/chairs"]').first().click()
+    cy.checkforPageLoad()
+    cy.checkUrlContains('/department/chairs')
+
+    // validation for out of stock chairs 
+    cy.getElementByHtml('.status.status--red', 'Sold out').click()
+    cy.getElementByHtml('.modal-sold-out--is-open > .modal-sold-out-content > .modal-button', 'Continue shopping').click()
+
+    // checking for the first chair in stock and adding to cart
+    util.addFirstElementInStockToCart()
+    cy.checkUrlContains('/chairs/product/')
+    util.assertCountInCart(1)
+
+    // goto sofa section
+    cy.get('a[href*="/department/sofas"]').first().click()
+    cy.checkUrlContains('department/sofas')
+
+    // checking for the first sofa in stock and adding to cart
+    util.addFirstElementInStockToCart()
+    cy.checkUrlContains('/sofas/product/')
+    util.assertCountInCart(2)
+
+    //verifying items in cart
+    cy.getElementByHtml('.cart > .menu-item-large-container > .menu-item-large', 'Cart (2)').click()
+    cy.get('.product-description').should('have.length', 2).each(($el, index) => {
+      cy.assertElementContainsText($el, this.input.productNames[index])
+      cy.assertElementContainsText($el, this.input.productPrice[index])
+    })
+
+    //assert total order value
+    cy.assertElementContainsText('.line-total', this.input.total)
+  })
+
+  it('Discount coupon test', function () {
+
+    cy.get('.cart').click()
+
+    // Adding random discount code
+    cy.get('.discount').type("asdad")
+    cy.getElementByHtml('.discount > div', 'Apply').click()
+
+    // assert error text
+    cy.assertElementText('.discount-toast', 'Coupon is invalid')
   })
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,31 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('getElementByHtml', (element, text) => {
+   return cy.get(element).first().should("be.visible").should('have.html', text);
+})
+
+
+Cypress.Commands.add('checkUrlContains', (url) => {
+   cy.url().should('include', url)
+})
+
+
+Cypress.Commands.add('checkforPageLoad', () => {
+   cy.intercept({
+      method: 'GET',
+      url: 'https://api.shopist.io/*',
+   }).as('productDataJson');
+   cy.wait('@productDataJson').its('response.statusCode').should('equal', 200)
+})
+
+Cypress.Commands.add('assertElementText', (element, text) => {
+   cy.get(element).should('have.text', text)
+})
+
+
+Cypress.Commands.add('assertElementContainsText', (element, text) => {
+   cy.get(element).should('include.text', text)
+})
