41792868b31f46ed54649ea015bbe329ddc9ed4c
SharanyaRK <sharanyaece75@gmail.com>
2022-01-10 14:07:45 +0530

    Delete utils.js

--- utils.js
+++
@@ -1,8 +0,0 @@
-export const addFirstElementInStockToCart = () => {
-    cy.getElementByHtml('.status', 'In stock').click()
-    cy.getElementByHtml('.purchase-button', 'Add to cart').click()
-}
-
-export const assertCountInCart = (count) => {
-    cy.assertElementText('.cart > .menu-item-large-container > .menu-item-large', 'Cart (' + count + ')')
-}
